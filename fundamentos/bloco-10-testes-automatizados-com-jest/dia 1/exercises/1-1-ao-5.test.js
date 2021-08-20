5208f1b25411834e9e142d2d6df791349c399b5a
vinicius-mns <vinicius_mns@hotmail.com>
2021-08-20 15:14:45 -0300

    bloco 10 dia 1 requisito 1-2

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia 1/exercises/1-1-ao-5.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia 1/exercises/1-1-ao-5.test.js
@@ -11,7 +11,7 @@ const {
 //  1.3 - Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 //  1.4 - Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
 
-describe('a funcao sun(a, b) retorna', () => {
+describe('A funcao sun(a, b) retorna', () => {
   it('9 se chamar sum(4, 5)', () => {
     expect(sum(4, 5)).toBe(9)
   })
@@ -28,3 +28,22 @@ describe('a funcao sun(a, b) retorna', () => {
     expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers'))
   })
 })
+
+
+// 2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+//  2.1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+//  2.2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+//  2.3 - Verifique se o array passado por parâmetro não sofreu alterações
+//  2.4 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+
+describe('A funcao myRemove(arr, item) retorna', () => {
+  const array = [1,2,3,4]
+
+  it('o array esperado', () => {
+    expect(myRemove(array, 3)).toEqual([1,2,4])
+  })
+
+  it('o array inalterado', () => {
+    expect(array).toEqual([1,2,3,4])
+  })
+}) 
