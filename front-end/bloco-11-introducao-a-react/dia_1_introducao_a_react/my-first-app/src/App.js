9c2266a34ea35e2e31c155dbfefdf0f7df3f2b74
vinicius-mns <vinicius_mns@hotmail.com>
2021-08-31 00:39:10 -0300

    fazendo o react funcionar

--- front-end/bloco-11-introducao-a-react/dia_1_introducao_a_react/my-first-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia_1_introducao_a_react/my-first-app/src/App.js
@@ -9,6 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+        Olha só que coisa mais top mano
+        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
