cd5fcc0a999a682f1279d06556bd299c5ea239a7
vinicius-mns <vinicius_mns@hotmail.com>
2021-08-31 02:17:38 -0300

    hello JSX giratorio

--- front-end/bloco-11-introducao-a-react/dia_1_introducao_a_react/my-first-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia_1_introducao_a_react/my-first-app/src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const element = "Hello, JSX"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="App-logo" alt="logo"> Hello world </p>
+        <p className="App-logo">
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <p>
+        <p className="App-logo">
         Olha só que coisa mais top mano
         </p>
 
+        <h1 className ="App-logo"> { element } </h1>
+
         <a
-          className="App-link"
+          className="App-link App-logo"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
