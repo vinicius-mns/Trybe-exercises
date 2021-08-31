7975c65ff8830b0cb84b7d725f53b5b642412533
vinicius-mns <vinicius_mns@hotmail.com>
2021-08-31 15:50:08 -0300

    dia 2 bloco 11 - exercicio de fixacao

---
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/app-componentes/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Image from './fixation-exercises-11-2';
+
+function App() {
+  return (
+    <div className="App">
+      <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"/>
+    </div>
+  );
+}
+
+export default App;
